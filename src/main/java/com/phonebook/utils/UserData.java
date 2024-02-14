8d41c4f2c0ca5e51a9c68ce23ecc33fcf321c7cf
Vyacheslav <my1304@t-online.de>
2024-02-14 17:21:20 +0100

    Initial commit

---
+++ src/main/java/com/phonebook/utils/UserData.java
@@ -0,0 +1,7 @@
+package com.phonebook.utils;
+
+public class UserData {
+
+    public static final String EMAIL = "nn04@gm.com";
+    public static final String PASSWORD = "Nnnnn1234$";
+}
---
+++ src/test/java/com/phonebook/tests/HomePageTests.java
@@ -0,0 +1,20 @@
+package com.phonebook.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class HomePageTests extends TestBase {
+    @BeforeMethod
+    public void ensuePrecondition() {
+        if (!app.getHomePage().isHomeComponentPresent()){
+            app.getHomePage().clickOnHomeLink();
+        }
+    }
+
+    @Test
+    public void isHomeComponentPresentTest() {
+        Assert.assertTrue(app.getHomePage().isHomeComponentPresent());
+    }
+}
